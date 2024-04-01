252713ebf523384152dca4ab22e8b4194d710711
vitpinheiro <vitoriapinheiro775@gmail.com>
2024-04-01 10:11:21 -0300

    adicionei uma imagem a pasta media

--- Loja/loja/models/Produto.py
+++ Loja/loja/models/Produto.py
@@ -14,7 +14,7 @@ related_name='fabricante', on_delete=models.SET_NULL)
 #loja/models/Produto.py parte 2:
     criado_em = models.DateTimeField(auto_now_add=True)
     alterado_em = models.DateTimeField(auto_now=True)
-    image = models.ImageField(null=true, blank=true)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return '{}'.format(self.Produto)    
