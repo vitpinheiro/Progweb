82ae8458ff6cb6871c743fc89bf7f7d1f2bd7942
Maria Vitória Rodrigues da Silva Pinheiro <vitoria.pinheiro@academico.ifrn.edu.br>
2024-08-16 11:05:17 -0300

    capitulo 7 finalizado

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,9 +1,10 @@
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render, redirect
-from loja.models import Produto
+from loja.models import Produto, Fabricante, Categoria
 from datetime import timedelta, datetime
 from django.utils import timezone
 # inclua as bibliotecas FileSystemStorage
-from django.core.files.storage import FileSystemStorage
+
 
 def list_produto_view(request, id=None):
     produto = request.GET.get("produto")
@@ -46,7 +47,9 @@ def edit_produto_view(request, id=None):
         produtos = produtos.filter(id=id)
     produto = produtos.first()
     print(produto)
-    context = { 'produto': produto }
+    Fabricantes = Fabricante.objects.all()
+    Categorias = Categoria.objects.all()
+    context = {'produto': produto, 'fabricantes' : Fabricantes, 'categorias' : Categorias}
     return render(request, template_name='produto/produto-edit.html', context=context,status=200)
 
 
@@ -60,6 +63,8 @@ def edit_produto_postback(request, id=None):
         destaque = request.POST.get("destaque")
         promocao = request.POST.get("promocao")
         msgPromocao = request.POST.get("msgPromocao")
+        categoria = request.POST.get("CategoriaFk")
+        fabricante = request.POST.get("FabricanteFk")
         print("postback")
         print(id)
         print(produto)
@@ -71,6 +76,9 @@ def edit_produto_postback(request, id=None):
             obj_produto.Produto = produto
             obj_produto.destaque = (destaque is not None)
             obj_produto.promocao = (promocao is not None)
+            #salve os objeto fabricante e categoria filtrados com base no id recebido na variavel categoria e fabricante
+            obj_produto.fabricante = Fabricante.objects.filter(id=fabricante).first()
+            obj_produto.categoria = Categoria.objects.filter(id=categoria).first()
             if msgPromocao is not None:
                 obj_produto.msgPromocao = msgPromocao
             obj_produto.save()
@@ -117,14 +125,15 @@ def delete_produto_postback(request, id=None):
     return redirect("/produto")
 
 def create_produto_view(request, id=None):
-# Processa o post back gerado pela action
+    # Processa o post back gerado pela action
     if request.method == 'POST':
         produto = request.POST.get("Produto")
         destaque = request.POST.get("destaque")
         promocao = request.POST.get("promocao")
         msgPromocao = request.POST.get("msgPromocao")
         preco = request.POST.get("preco")
-        image = request.POST.get("image")
+        image = request.FILES.get("image")  # Obtendo diretamente o arquivo
+        
         print("postback-create")
         print(produto)
         print(destaque)
@@ -132,31 +141,31 @@ def create_produto_view(request, id=None):
         print(msgPromocao)
         print(preco)
         print(image)
+        
         try:
             obj_produto = Produto()
             obj_produto.Produto = produto
             obj_produto.destaque = (destaque is not None)
             obj_produto.promocao = (promocao is not None)
-            if msgPromocao is not None:
+            if msgPromocao:
                 obj_produto.msgPromocao = msgPromocao
             obj_produto.preco = 0
-            if (preco is not None) and ( preco != ""):
+            if preco:
                 obj_produto.preco = preco
             obj_produto.criado_em = timezone.now()
             obj_produto.alterado_em = obj_produto.criado_em
-# Se for anexado arquivo, salva na pasta e guarda nome no objeto
-            if request.FILES is not None:
-                num_files = len(request.FILES.getlist('image'))
-            if num_files > 0:
-                imagefile = request.FILES['image']
-                print(imagefile)
+
+            # Se for anexado arquivo, salva na pasta e guarda nome no objeto
+            if image:
                 fs = FileSystemStorage()
-                filename = fs.save(imagefile.name, imagefile)
-            if (filename is not None) and (filename != ""):
-                obj_produto.image = filename
+                filename = fs.save(image.name, image)  # Salvando o arquivo
+                obj_produto.image = filename  # Atribuindo o nome do arquivo ao objeto
+
             obj_produto.save()
-            print("Produto %s salvo com sucesso" % produto)
+            print(f"Produto {produto} salvo com sucesso")
         except Exception as e:
-            print("Erro inserindo produto: %s" % e)
+            print(f"Erro inserindo produto: {e}")
+        
         return redirect("/produto")
-    return render(request, template_name='produto/produto-create.html',status=200)
+    
+    return render(request, template_name='produto/produto-create.html', status=200)
