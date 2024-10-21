d224558e5a45154dbd4b802e8ab47327f54afe00
Maria Vitória Rodrigues da Silva Pinheiro <vitoria.pinheiro@academico.ifrn.edu.br>
2024-10-21 10:23:16 -0300

    cap 9 concluído

--- Loja/loja/views/UsuarioView.py
+++ Loja/loja/views/UsuarioView.py
@@ -1,22 +1,49 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from loja.models import Usuario
 from loja.forms.UserUsuarioForm import UserUsuarioForm, UserForm
+
 def list_usuario_view(request, id=None):
-# carrega somente usuarios, não inclui os admin
+    # Carrega somente usuários, não inclui os admin
     usuarios = Usuario.objects.filter(perfil=2)
     context = {
-    'usuarios': usuarios
+        'usuarios': usuarios
     }
-    return render(request, template_name='usuario/usuario.html', context=context,status=200)
+    return render(request, template_name='usuario/usuario.html', context=context, status=200)
 
-# adicione o método de edição
+# Adicione o método de edição
 def edit_usuario_view(request):
     usuario = get_object_or_404(Usuario, user=request.user)
-    usuarioForm = UserUsuarioForm(instance=usuario)
-    # Crie uma instância da classe UserForm
-    userForm = UserForm(instance=request.user)
+    emailUnused = True
+    message = None
+    
+    if request.method == 'POST':
+        usuarioForm = UserUsuarioForm(request.POST, instance=usuario)
+        userForm = UserForm(request.POST, instance=request.user)
+        verifyEmail = Usuario.objects.filter(user__email=request.POST['email']).exclude(user__id=request.user.id).first()
+        emailUnused = verifyEmail is None
+    else:
+        usuarioForm = UserUsuarioForm(instance=usuario)
+        userForm = UserForm(instance=request.user)
+
+    if usuarioForm.is_valid() and userForm.is_valid() and emailUnused:
+        usuarioForm.save()
+        userForm.save()
+        message = {'type': 'success', 'text': 'Dados atualizados com sucesso'}
+    else:
+        # Aqui verificamos se é do tipo POST, para que na primeira vez que a página
+        # carregar a mensagem não apareça, já que no primeiro carregamento não enviamos um POST.
+        if request.method == 'POST':
+            if emailUnused:
+                # Se o e-mail não está em uso, tiver algum dado inválido.
+                message = {'type': 'danger', 'text': 'Dados inválidos'}
+            else:
+                # Se o e-mail já está em uso por outra pessoa.
+                message = {'type': 'warning', 'text': 'E-mail já usado'}
+
     context = {
-    'usuarioForm': usuarioForm,
-    'userForm': userForm
+        'usuarioForm': usuarioForm,
+        'userForm': userForm,
+        'message': message
     }
-    return render(request, template_name='usuario/usuario-edit.html', context=context,status=200)
+    
+    return render(request, template_name='usuario/usuario-edit.html', context=context, status=200)
